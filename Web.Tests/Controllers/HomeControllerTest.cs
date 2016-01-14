cb3977d860ed6afd692f8db60e676454b19b8c40
unknown <linh.tran@S60-042.lanworks.local>
2016-01-14 10:21:07 +0700

    automation test register and login

---
+++ Web.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Web;
+using Web.Controllers;
+using OpenQA.Selenium.Chrome;
+
+namespace Web.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        ChromeDriver chromeDrive;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            chromeDrive = new ChromeDriver(ConfigurationHelper.ChromeDrive);
+        }
+
+        [TestMethod]
+        public void RegisterSuccess()
+        {
+            chromeDrive.Navigate().GoToUrl(ConfigurationHelper.RegisterUrl);
+            var txtUserName = chromeDrive.FindElementById("UserName");
+            var txtPassword = chromeDrive.FindElementById("Password");
+            var txtConfirmPassword = chromeDrive.FindElementById("ConfirmPassword");
+            var btnRegister = chromeDrive.FindElementById("Register");
+
+            txtUserName.SendKeys(ConfigurationHelper.TestUserName);
+            txtPassword.SendKeys(ConfigurationHelper.TestPassword);
+            txtConfirmPassword.SendKeys(ConfigurationHelper.TestPassword);
+
+            btnRegister.Submit();
+
+            chromeDrive.Navigate().GoToUrl(ConfigurationHelper.LoginUrl);
+            bool result = false;
+            try
+            {
+                var logoutForm = chromeDrive.FindElementById("logoutForm");
+                result = logoutForm.Displayed;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void LoginSuccess()
+        {
+            chromeDrive.Navigate().GoToUrl(ConfigurationHelper.LoginUrl);
+            var txtUserName = chromeDrive.FindElementById("UserName");
+            var txtPassword = chromeDrive.FindElementById("Password");
+            var btnLogin = chromeDrive.FindElementById("Login");
+
+            txtUserName.SendKeys(ConfigurationHelper.TestUserName);
+            txtPassword.SendKeys(ConfigurationHelper.TestPassword);
+
+            btnLogin.Submit();
+
+            chromeDrive.Navigate().GoToUrl(ConfigurationHelper.LoginUrl);
+            bool result = false;
+            try
+            {
+                var logoutForm = chromeDrive.FindElementById("logoutForm");
+                result = logoutForm.Displayed;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void RegisterFailure()
+        {
+            chromeDrive.Navigate().GoToUrl(ConfigurationHelper.RegisterUrl);
+            var txtUserName = chromeDrive.FindElementById("UserName");
+            var txtPassword = chromeDrive.FindElementById("Password");
+            var txtConfirmPassword = chromeDrive.FindElementById("ConfirmPassword");
+            var btnRegister = chromeDrive.FindElementById("Register");
+
+            txtUserName.SendKeys(ConfigurationHelper.TestUserName);
+            txtPassword.SendKeys(ConfigurationHelper.TestPassword);
+            txtConfirmPassword.SendKeys(ConfigurationHelper.TestPassword);
+
+            btnRegister.Submit();
+
+            chromeDrive.Navigate().GoToUrl(ConfigurationHelper.LoginUrl);
+            bool result = false;
+            try
+            {
+                var logoutForm = chromeDrive.FindElementById("logoutForm");
+                result = logoutForm.Displayed;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            Assert.AreEqual(result, false);
+        }
+
+        [TestMethod]
+        public void LoginFailure()
+        {
+            chromeDrive.Navigate().GoToUrl(ConfigurationHelper.LoginUrl);
+            var txtUserName = chromeDrive.FindElementById("UserName");
+            var txtPassword = chromeDrive.FindElementById("Password");
+            var btnLogin = chromeDrive.FindElementById("Login");
+
+            txtUserName.SendKeys("linh.tran");
+            txtPassword.SendKeys("123456");
+
+            btnLogin.Submit();
+
+            chromeDrive.Navigate().GoToUrl(ConfigurationHelper.LoginUrl);
+            bool result = false;
+            try
+            {
+                var logoutForm = chromeDrive.FindElementById("logoutForm");
+                result = logoutForm.Displayed;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            Assert.AreEqual(result, false);
+        }
+    }
+}
---
+++ Web.Tests/Helper/ConfigurationHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Tests
+{
+    public static class ConfigurationHelper
+    {
+        public static string SiteUrl
+        {
+            get { return ConfigurationManager.AppSettings["SiteUrl"]; }
+        }
+
+        public static string RegisterUrl
+        {
+            get { return string.Format("{0}{1}", SiteUrl, ConfigurationManager.AppSettings["RegisterUrl"]); }
+        }
+
+        public static string LoginUrl
+        {
+            get { return string.Format("{0}{1}", SiteUrl, ConfigurationManager.AppSettings["LoginUrl"]); }
+        }
+
+        public static string ChromeDrive
+        {
+            get { return ConfigurationManager.AppSettings["ChromeDrive"]; }
+        }
+
+        public static string TestUserName
+        {
+            get { return ConfigurationManager.AppSettings["TestUserName"]; }
+        }
+
+        public static string TestPassword
+        {
+            get { return ConfigurationManager.AppSettings["TestPassword"]; }
+        }
+    }
+}
